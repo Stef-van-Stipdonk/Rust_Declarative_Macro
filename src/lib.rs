a49f4ec7fe391845811f4c63978d72e66301d07c
Stef <stefvanstipdonk@gmail.com>
2022-10-03 12:37:38 +0200

    Fix test to pass again

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@ fn single_vec() {
 fn triple_vec() {
     let x: Vec<u32> = vec2![42, 43, 44];
     assert!(!x.is_empty());
-    assert_eq!(x.len(), 2);
+    assert_eq!(x.len(), 3);
     assert_eq!(x[0], 42);
     assert_eq!(x[1], 43);
     assert_eq!(x[2], 44)
