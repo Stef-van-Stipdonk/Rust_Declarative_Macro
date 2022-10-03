7797dab760724e6338346ab55d58ad0f733b5eec
Stef <stefvanstipdonk@gmail.com>
2022-10-03 10:53:52 +0200

    auto_generate_vec_of_size_with_value

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,20 @@ macro_rules! vec2 {
     // + means 1 or more separate expressions.
     // * means 0 or more.
     // $(,)? means that after every expression 0 or 1 commas can be present.
-    ($($element:expr),+ $(,)?) => {{
+    ($($value:expr),+) => {{
         let mut vs = Vec::new();
-        $(vs.push($element);)*
+        $(vs.push($value);)*
         vs
     }};
+
+    ($value:expr; $amount:expr) => {{
+        let mut vs = Vec::with_capacity($amount);
+        for i in 0..$amount {
+            vs.push($value);
+        }
+
+        vs
+    }}
 }
 
 #[test]
@@ -30,15 +39,26 @@ fn single_vec() {
 }
 
 #[test]
-fn double_vec() {
-    let x: Vec<u32> = vec2![42, 43];
+fn triple_vec() {
+    let x: Vec<u32> = vec2![42, 43, 44];
     assert!(!x.is_empty());
     assert_eq!(x.len(), 2);
     assert_eq!(x[0], 42);
     assert_eq!(x[1], 43);
+    assert_eq!(x[2], 44)
 }
 
 #[test]
 fn trailing_comma_vec() {
-    let x: Vec<u32> = vec2![1, 2, 3, 4, 5, 6, 7,];
+    let x: Vec<u32> = vec2![1, 2, 3, 4, 5, 6, 7];
+}
+
+#[test]
+fn auto_generate_vec_of_size_with_value() {
+    let x: Vec<u32> = vec2!(5; 3);
+
+    assert_eq!(x.len(), 3);
+    assert_eq!(x[0], 5);
+    assert_eq!(x[1], 5);
+    assert_eq!(x[2], 5);
 }
